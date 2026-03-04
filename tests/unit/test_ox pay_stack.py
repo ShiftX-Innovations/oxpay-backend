0a742f47144c70edd3a62281cbbe4b700d37b84b
DilshanMuditha <mudithadilshan1999@gmail.com>
2026-03-04 12:05:05 +0530

    Initial commit

---
+++ tests/unit/test_ox pay_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from ox pay.ox pay_stack import OxPayStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ox pay/ox pay_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = OxPayStack(app, "ox-pay")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
